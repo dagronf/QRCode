20dcbe08e6ca522844f21d2c1c21e7cdc60e6d78
Darren Ford <dford_au-reg@yahoo.com>
2023-04-05 15:24:23 +1000

    Fixed iOS compile error

--- Sources/QRCode/styles/pupil/QRCodePupilShapeShield.swift
+++ Sources/QRCode/styles/pupil/QRCodePupilShapeShield.swift
@@ -102,96 +102,96 @@ public extension QRCode.PupilShape.Shield {
 
 	func trIN() -> CGPath {
 		let trINPath = CGMutablePath()
-		trINPath.move(to: NSPoint(x: 52.42, y: 60))
-		trINPath.curve(to: NSPoint(x: 60, y: 52.42), controlPoint1: NSPoint(x: 53.97, y: 56.66), controlPoint2: NSPoint(x: 56.66, y: 53.97))
-		trINPath.curve(to: NSPoint(x: 60, y: 45), controlPoint1: NSPoint(x: 60, y: 48.7), controlPoint2: NSPoint(x: 60, y: 45))
-		trINPath.line(to: NSPoint(x: 45, y: 45))
-		trINPath.line(to: NSPoint(x: 45, y: 60))
-		trINPath.line(to: NSPoint(x: 52.42, y: 60))
+		trINPath.move(to: CGPoint(x: 52.42, y: 60))
+		trINPath.curve(to: CGPoint(x: 60, y: 52.42), controlPoint1: CGPoint(x: 53.97, y: 56.66), controlPoint2: CGPoint(x: 56.66, y: 53.97))
+		trINPath.curve(to: CGPoint(x: 60, y: 45), controlPoint1: CGPoint(x: 60, y: 48.7), controlPoint2: CGPoint(x: 60, y: 45))
+		trINPath.line(to: CGPoint(x: 45, y: 45))
+		trINPath.line(to: CGPoint(x: 45, y: 60))
+		trINPath.line(to: CGPoint(x: 52.42, y: 60))
 		trINPath.close()
 		return trINPath
 	}
 
 	func trOUT() -> CGPath {
 		let trOUTPath = CGMutablePath()
-		trOUTPath.move(to: NSPoint(x: 45, y: 45))
-		trOUTPath.line(to: NSPoint(x: 60, y: 45))
-		trOUTPath.line(to: NSPoint(x: 60, y: 56))
-		trOUTPath.curve(to: NSPoint(x: 56, y: 60), controlPoint1: NSPoint(x: 60, y: 58.21), controlPoint2: NSPoint(x: 58.21, y: 60))
-		trOUTPath.line(to: NSPoint(x: 45, y: 60))
-		trOUTPath.line(to: NSPoint(x: 45, y: 45))
+		trOUTPath.move(to: CGPoint(x: 45, y: 45))
+		trOUTPath.line(to: CGPoint(x: 60, y: 45))
+		trOUTPath.line(to: CGPoint(x: 60, y: 56))
+		trOUTPath.curve(to: CGPoint(x: 56, y: 60), controlPoint1: CGPoint(x: 60, y: 58.21), controlPoint2: CGPoint(x: 58.21, y: 60))
+		trOUTPath.line(to: CGPoint(x: 45, y: 60))
+		trOUTPath.line(to: CGPoint(x: 45, y: 45))
 		trOUTPath.close()
 		return trOUTPath
 	}
 
 	func tlIN() -> CGPath {
 		let tlINPath = CGMutablePath()
-		tlINPath.move(to: NSPoint(x: 37.58, y: 60))
-		tlINPath.curve(to: NSPoint(x: 30, y: 52.42), controlPoint1: NSPoint(x: 36.03, y: 56.66), controlPoint2: NSPoint(x: 33.34, y: 53.97))
-		tlINPath.curve(to: NSPoint(x: 30, y: 45), controlPoint1: NSPoint(x: 30, y: 48.7), controlPoint2: NSPoint(x: 30, y: 45))
-		tlINPath.line(to: NSPoint(x: 45, y: 45))
-		tlINPath.line(to: NSPoint(x: 45, y: 60))
-		tlINPath.line(to: NSPoint(x: 37.58, y: 60))
+		tlINPath.move(to: CGPoint(x: 37.58, y: 60))
+		tlINPath.curve(to: CGPoint(x: 30, y: 52.42), controlPoint1: CGPoint(x: 36.03, y: 56.66), controlPoint2: CGPoint(x: 33.34, y: 53.97))
+		tlINPath.curve(to: CGPoint(x: 30, y: 45), controlPoint1: CGPoint(x: 30, y: 48.7), controlPoint2: CGPoint(x: 30, y: 45))
+		tlINPath.line(to: CGPoint(x: 45, y: 45))
+		tlINPath.line(to: CGPoint(x: 45, y: 60))
+		tlINPath.line(to: CGPoint(x: 37.58, y: 60))
 		tlINPath.close()
 		return tlINPath
 	}
 
 	func tlOUT() -> CGPath {
 		let tlOUTPath = CGMutablePath()
-		tlOUTPath.move(to: NSPoint(x: 45, y: 45))
-		tlOUTPath.line(to: NSPoint(x: 30, y: 45))
-		tlOUTPath.line(to: NSPoint(x: 30, y: 56))
-		tlOUTPath.curve(to: NSPoint(x: 34, y: 60), controlPoint1: NSPoint(x: 30, y: 58.21), controlPoint2: NSPoint(x: 31.79, y: 60))
-		tlOUTPath.line(to: NSPoint(x: 45, y: 60))
-		tlOUTPath.line(to: NSPoint(x: 45, y: 45))
+		tlOUTPath.move(to: CGPoint(x: 45, y: 45))
+		tlOUTPath.line(to: CGPoint(x: 30, y: 45))
+		tlOUTPath.line(to: CGPoint(x: 30, y: 56))
+		tlOUTPath.curve(to: CGPoint(x: 34, y: 60), controlPoint1: CGPoint(x: 30, y: 58.21), controlPoint2: CGPoint(x: 31.79, y: 60))
+		tlOUTPath.line(to: CGPoint(x: 45, y: 60))
+		tlOUTPath.line(to: CGPoint(x: 45, y: 45))
 		tlOUTPath.close()
 		return tlOUTPath
 	}
 
 	func brIN() -> CGPath {
 		let brINPath = CGMutablePath()
-		brINPath.move(to: NSPoint(x: 52.42, y: 30))
-		brINPath.curve(to: NSPoint(x: 60, y: 37.58), controlPoint1: NSPoint(x: 53.97, y: 33.34), controlPoint2: NSPoint(x: 56.66, y: 36.03))
-		brINPath.curve(to: NSPoint(x: 60, y: 45), controlPoint1: NSPoint(x: 60, y: 41.3), controlPoint2: NSPoint(x: 60, y: 45))
-		brINPath.line(to: NSPoint(x: 45, y: 45))
-		brINPath.line(to: NSPoint(x: 45, y: 30))
-		brINPath.line(to: NSPoint(x: 52.42, y: 30))
+		brINPath.move(to: CGPoint(x: 52.42, y: 30))
+		brINPath.curve(to: CGPoint(x: 60, y: 37.58), controlPoint1: CGPoint(x: 53.97, y: 33.34), controlPoint2: CGPoint(x: 56.66, y: 36.03))
+		brINPath.curve(to: CGPoint(x: 60, y: 45), controlPoint1: CGPoint(x: 60, y: 41.3), controlPoint2: CGPoint(x: 60, y: 45))
+		brINPath.line(to: CGPoint(x: 45, y: 45))
+		brINPath.line(to: CGPoint(x: 45, y: 30))
+		brINPath.line(to: CGPoint(x: 52.42, y: 30))
 		brINPath.close()
 		return brINPath
 	}
 
 	func brOUT() -> CGPath {
 		let brOUTPath = CGMutablePath()
-		brOUTPath.move(to: NSPoint(x: 45, y: 45))
-		brOUTPath.line(to: NSPoint(x: 60, y: 45))
-		brOUTPath.line(to: NSPoint(x: 60, y: 34))
-		brOUTPath.curve(to: NSPoint(x: 56, y: 30), controlPoint1: NSPoint(x: 60, y: 31.79), controlPoint2: NSPoint(x: 58.21, y: 30))
-		brOUTPath.line(to: NSPoint(x: 45, y: 30))
-		brOUTPath.line(to: NSPoint(x: 45, y: 45))
+		brOUTPath.move(to: CGPoint(x: 45, y: 45))
+		brOUTPath.line(to: CGPoint(x: 60, y: 45))
+		brOUTPath.line(to: CGPoint(x: 60, y: 34))
+		brOUTPath.curve(to: CGPoint(x: 56, y: 30), controlPoint1: CGPoint(x: 60, y: 31.79), controlPoint2: CGPoint(x: 58.21, y: 30))
+		brOUTPath.line(to: CGPoint(x: 45, y: 30))
+		brOUTPath.line(to: CGPoint(x: 45, y: 45))
 		brOUTPath.close()
 		return brOUTPath
 	}
 
 	func blIN() -> CGPath {
 		let blINPath = CGMutablePath()
-		blINPath.move(to: NSPoint(x: 37.58, y: 30))
-		blINPath.curve(to: NSPoint(x: 30, y: 37.58), controlPoint1: NSPoint(x: 36.03, y: 33.34), controlPoint2: NSPoint(x: 33.34, y: 36.03))
-		blINPath.curve(to: NSPoint(x: 30, y: 45), controlPoint1: NSPoint(x: 30, y: 41.3), controlPoint2: NSPoint(x: 30, y: 45))
-		blINPath.line(to: NSPoint(x: 45, y: 45))
-		blINPath.line(to: NSPoint(x: 45, y: 30))
-		blINPath.line(to: NSPoint(x: 37.58, y: 30))
+		blINPath.move(to: CGPoint(x: 37.58, y: 30))
+		blINPath.curve(to: CGPoint(x: 30, y: 37.58), controlPoint1: CGPoint(x: 36.03, y: 33.34), controlPoint2: CGPoint(x: 33.34, y: 36.03))
+		blINPath.curve(to: CGPoint(x: 30, y: 45), controlPoint1: CGPoint(x: 30, y: 41.3), controlPoint2: CGPoint(x: 30, y: 45))
+		blINPath.line(to: CGPoint(x: 45, y: 45))
+		blINPath.line(to: CGPoint(x: 45, y: 30))
+		blINPath.line(to: CGPoint(x: 37.58, y: 30))
 		blINPath.close()
 		return blINPath
 	}
 
 	func blOUT() -> CGPath {
 		let blOUTPath = CGMutablePath()
-		blOUTPath.move(to: NSPoint(x: 45, y: 45))
-		blOUTPath.line(to: NSPoint(x: 30, y: 45))
-		blOUTPath.line(to: NSPoint(x: 30, y: 34))
-		blOUTPath.curve(to: NSPoint(x: 34, y: 30), controlPoint1: NSPoint(x: 30, y: 31.79), controlPoint2: NSPoint(x: 31.79, y: 30))
-		blOUTPath.line(to: NSPoint(x: 45, y: 30))
-		blOUTPath.line(to: NSPoint(x: 45, y: 45))
+		blOUTPath.move(to: CGPoint(x: 45, y: 45))
+		blOUTPath.line(to: CGPoint(x: 30, y: 45))
+		blOUTPath.line(to: CGPoint(x: 30, y: 34))
+		blOUTPath.curve(to: CGPoint(x: 34, y: 30), controlPoint1: CGPoint(x: 30, y: 31.79), controlPoint2: CGPoint(x: 31.79, y: 30))
+		blOUTPath.line(to: CGPoint(x: 45, y: 30))
+		blOUTPath.line(to: CGPoint(x: 45, y: 45))
 		blOUTPath.close()
 		return blOUTPath
 	}
