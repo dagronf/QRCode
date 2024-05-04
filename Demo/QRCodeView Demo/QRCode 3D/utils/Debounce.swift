cc5e181d73bf8d8f9efef1f5f85dbac70d416137
Darren Ford <dford_au-reg@yahoo.com>
2024-05-04 11:17:15 +1000

    Rename demo projects for clarity

---
+++ Demo/QRCodeView Demo/QRCode 3D/utils/Debounce.swift
@@ -0,0 +1,44 @@
+//
+//  Debounce.swift
+//
+//  Copyright © 2024 Darren Ford. All rights reserved.
+//
+//  MIT license
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+//  documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+//  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+//  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import Foundation
+import Dispatch
+
+internal class DSFDebounce {
+
+	// MARK: - Properties
+	private let queue: DispatchQueue
+	private var workItem = DispatchWorkItem(block: {})
+	private var interval: TimeInterval
+
+	// MARK: - Initializer
+	init(seconds: TimeInterval, queue: DispatchQueue = DispatchQueue.main) {
+		self.interval = seconds
+		self.queue = queue
+	}
+
+	// MARK: - Debouncing function
+	func debounce(action: @escaping (() -> Void)) {
+		self.workItem.cancel()
+		self.workItem = DispatchWorkItem(block: { action() })
+		self.queue.asyncAfter(deadline: .now() + interval, execute: workItem)
+	}
+}
